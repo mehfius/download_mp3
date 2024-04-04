f4428f9f2f51c1efcb7c13816f9f2222147714da
mehfius <matheus.ferraz@gmail.com>
2024-04-04 00:40:35 +0000

    .

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+// Obtenha o URL do vídeo do YouTube
+const url = 'https://www.youtube.com/watch?v=vglkukcW6KQ';
+
+//const url = 'https://www.youtube.com/watch?v=RebnxklaFd0';
+
+// Crie um stream de áudio do vídeo
+const stream = ytdl(url, {
+  filter: 'audioonly',
+  format: 'mp3',
+});
+
+// Salve o stream de áudio em um arquivo
+stream.pipe(fs.createWriteStream('audio.mp3'));
